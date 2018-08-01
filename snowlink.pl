2f88d3d19f358024dc76648b650c5c37d609aa53
Joe Pusateri <jpusateri@Joes-MacBook-Pro-2.local>
2018-07-31 20:15:32 -0500

    Read from CSV

--- snowlink.pl
+++ snowlink.pl
@@ -33,10 +33,13 @@ use JSON;
 use Data::Dumper;
 use URI::Escape;
 use strict;
+use Text::CSV;
 
 my(%opts);
 my(@opts)=('pagerduty_token|p=s',
+           'filename|f=s',
            'help|h',
+           'debug|d'
     );
 
 die unless GetOptions(\%opts,@opts);
@@ -48,48 +51,45 @@ $0: get relevant keys for ServiceNow Extensions
 options:
 
  --pagerduty_token | -p <V2 API token>
+ --file | f <filename>
  --help | -h (this message)
+ --debug | -d
 EOT
 exit 0;
 }
 
 die "--pagerduty_token|-p required" unless($opts{pagerduty_token});
+die "--filename|-f required" unless($opts{filename});
 
 #faked inputs
-my($parm_snow_instance) = "dev52132";
-my($parm_snow_url_target) = "https://$parm_snow_instance.service-now.com/api/x_pd_integration/pagerduty2sn";
-my($parm_snow_api_user) = "pagerduty-int";
-my($parm_snow_api_pw) = "PagerDuty";
-my($parm_snow_update_user) = "pagerduty-int";
-my($parm_snow_update_pw) = "PagerDuty";
-
-my($parm_service_target) = "AddResponderCallout";
-my($parm_snow_assignment_group_name) = "Change Management";
-
 my($j, $cmd);
+my(%int_version);
+$int_version{'v4'} = "P6MB86H";
+$int_version{'v5'} = "PAD6MYW";
 
 sub getService {
   my($service_target) = @_;
   my(%service);
+  $service{'serviceid'} = "";
+  $service{'epid'} = "";
   # retrieve specified service
   $cmd = "curl -s -H 'Authorization: Token token=$opts{pagerduty_token}' " .
       "'https://api.pagerduty.com/services?query=@{[uri_escape($service_target)]}'";
   print "$cmd\n" if($opts{debug});
   $j = scalar(`$cmd`);
   my($as) = from_json($j, {allow_nonref=>1});
-
+  print "$j\n" if($opts{debug});
   for(@{$as->{services}}){
     $service{'serviceid'} = $_->{id};
     $service{'epid'} = $_->{escalation_policy}{id};
     last;
   }
   return %service;
-
 }
 
 sub findExistingExtension {
-  my($snow_url_target, $snow_service_target) = @_;
-  print "Looking for: $snow_url_target $snow_service_target\n";
+  my($snow_url_target, $snow_service_target, $int_version_key) = @_;
+  print "Looking for: $snow_url_target $snow_service_target\n" if($opts{debug});
   # look for a service that matches inputs
   my ($found_extension);
   $cmd = "curl -s -H 'Authorization: Token token=$opts{pagerduty_token}' " .
@@ -105,10 +105,10 @@ sub findExistingExtension {
       my($snow_version) = $_->{extension_schema}{id};
       my($snow_service) = $_->{extension_objects}[0]{id};
       my($snow_url) = $_->{config}{target};
-      print "$name $snow_user $snow_version $snow_url $snow_service\n";
-      if ($snow_version eq 'P6MB86H' && $snow_url eq $snow_url_target && $snow_service eq $snow_service_target)
+      print "Testing: $name $snow_user $snow_version $snow_url $snow_service\n" if($opts{debug});
+      if ($snow_version eq $int_version{$int_version_key} && $snow_url eq $snow_url_target && $snow_service eq $snow_service_target)
       {
-        print "compare succeeded\n";
+        print "compare succeeded\n" if($opts{debug});
         $found_extension = $_;
         last;
       }
@@ -119,20 +119,20 @@ sub findExistingExtension {
 
 
 sub createExtension {
-  my($snow_instance, $snow_api_user, $snow_api_pw, %service) = @_;
+  my($snow_instance, $snow_api_user, $snow_api_pw, $sync, $int_version_key, %service) = @_;
   # create extension
-  print "creating extension\n";
+  print "Creating extension $int_version_key\n" if($opts{debug});
   my($body) = {
     'extension' => {
       'name' => "ServiceNow ($snow_instance)",
       'config' => {
         "snow_user" =>  $snow_api_user,
         "snow_password" =>  $snow_api_pw,
-        "sync_options" =>  "manual_sync",
+        "sync_options" =>  ($sync eq "auto" ? "sync_all" : "manual_sync"),
         "target"=> "https://$snow_instance.service-now.com/api/x_pd_integration/pagerduty2sn"
       },
       "extension_schema" => {
-        "id" => "P6MB86H",
+        "id" => $int_version{$int_version_key},
         "type" => "extension_schema_reference"
       },
       "extension_objects" => [
@@ -144,30 +144,30 @@ sub createExtension {
     }
   };
   my $jsonbody = to_json $body;
-  print "JSON=$jsonbody\n";
+  print "Sending: $jsonbody\n" if($opts{debug});
 
   $cmd = "curl -s -H 'Authorization: Token token=$opts{pagerduty_token}' " .
       "-H 'Content-type: application/json' -X POST " .
       "-d '$jsonbody'" .
       " 'https://api.pagerduty.com/extensions'";
-  print "$cmd\n";
+  print "$cmd\n" if($opts{debug});
   $j = scalar(`$cmd`);
   my($ext_create) = from_json($j, {allow_nonref=>1});
   my($found_webhook_id) = $ext_create->{extension}{id};
-  print "\n$j\n";
-  print "create: found $service{'serviceid'} $service{'epid'} $found_webhook_id\n\n";
+  print "$j\n" if($opts{debug});
+  print "Created $service{'serviceid'} $service{'epid'} $found_webhook_id\n" if($opts{debug});
   return $found_webhook_id;
 }
 
 sub updateAssignmentGroup {
   my($snow_update_user, $snow_update_password, $snow_assignment_group_name, $snow_instance, $webhook_id, %service) = @_;
-  print "Updating Assignment Group $snow_assignment_group_name $webhook_id\n";
+  print "Updating Assignment Group $snow_assignment_group_name\n" if($opts{debug});
   #get sys_id for SN Assignment Group
   # $cmd = "curl -s -H 'Authorization: basic cGFnZXJkdXR5LWludDpQYWdlckR1dHk=' " .
   $cmd = "curl -s -u $snow_update_user:$snow_update_password " .
       "-H 'Accept: application/json' " .
       "'https://$snow_instance.service-now.com/api/now/table/sys_user_group?sysparm_query=name=@{[uri_escape($snow_assignment_group_name)]}'";
-  print "$cmd\n";
+  print "$cmd\n" if($opts{debug});
   $j = scalar(`$cmd`);
   my($sag) = from_json($j, {allow_nonref=>1});
   my($snow_ag_id) = $sag->{result}[0]{sys_id};
@@ -175,7 +175,7 @@ sub updateAssignmentGroup {
   if ($snow_ag_id)
   {
     #push SN Assignment Group update
-
+    print "Found Group\n" if($opts{debug});
     my($snow_ag_body) = {
       "x_pd_integration_pagerduty_webhook" => $webhook_id,
       "x_pd_integration_pagerduty_escalation" => $service{'epid'},
@@ -187,38 +187,77 @@ sub updateAssignmentGroup {
         "-H 'Content-Type: application/json' " .
         "-d '$jsonbody' " .
         "'https://$snow_instance.service-now.com/api/now/table/sys_user_group/$snow_ag_id'";
-    print "$cmd\n";
+    print "$cmd\n" if($opts{debug});
     $j = scalar(`$cmd`);
     my($sag_update) = from_json($j, {allow_nonref=>1});
-    print "$j\n";
+    print "$j\n" if($opts{debug});
+    print "Successfully updated $snow_assignment_group_name\n";
   }
   else
   {
     print "Unable to find Assignment Group $snow_assignment_group_name\n";
   }
 }
+#my($parm_snow_instance) = "dev52132";
+#my($parm_snow_api_user) = "pagerduty-int";
+#my($parm_snow_api_pw) = "PagerDuty";
+#my($parm_snow_update_user) = "pagerduty-int";
+#my($parm_snow_update_pw) = "PagerDuty";
 
-#get hash with service id and escalation policy id
-my(%service) = getService($parm_service_target);
-print "serviceid=$service{'serviceid'}\n";
-print "epid=$service{'epid'}\n";
+#y($parm_service_target) = "AddResponderCallout";
+#my($parm_snow_assignment_group_name) = "Change Management";
+my $csv = Text::CSV->new({ sep_char => ',' });
 
-#using service id, find if PD has an extension already
-my($found_extension) = findExistingExtension($parm_snow_url_target, $service{'serviceid'});
+my $file = $opts{filename};
+print "file is $file\n";
 
-my($found_webhook_id);
+open(my $data, '<', $file) or die "Could not open '$file' $!\n";
+while (my $line = <$data>) {
+  chomp $line;
 
-# if found, get webhook id
-if ($found_extension)
-{
-  $found_webhook_id = $found_extension->{id};
-  print "found $service{'serviceid'} $service{'epid'} $found_webhook_id\n\n";
+  if ($csv->parse($line)) {
+
+      my @fields = $csv->fields();
+      print("Read line: $fields[0], $fields[1], $fields[2], $fields[3], $fields[4], $fields[5], $fields[6], $fields[7], $fields[8]\n") if($opts{debug});
+      pair($fields[0], $fields[1], $fields[2], $fields[3], $fields[4], $fields[5], $fields[6], $fields[7], $fields[8]);
+
+  } else {
+      warn "Line could not be parsed: $line\n";
+  }
 }
-else
-{
-  $found_webhook_id = createExtension($parm_snow_instance, $parm_snow_api_user, $parm_snow_api_pw, %service);
 
+sub pair {
+  my($parm_snow_instance, $parm_snow_api_user, $parm_snow_api_pw,
+     $parm_snow_update_user, $parm_snow_update_pw, $parm_service_target,
+     $parm_snow_assignment_group_name, $parm_sync, $int_version_key)=@_;
+  #get hash with service id and escalation policy id
+  my($snow_url_target) = "https://$parm_snow_instance.service-now.com/api/x_pd_integration/pagerduty2sn";
+  my(%service) = getService($parm_service_target);
+  print "serviceid=$service{'serviceid'}\n" if($opts{debug});
+  print "epid     =$service{'epid'}\n" if($opts{debug});
+
+  if ($service{'serviceid'} eq "")
+  {
+    print "Service ID not found for $parm_service_target. Skipping...\n";
+    last;
+  }
+  #using service id, find if PD has an extension already
+  my($found_extension) = findExistingExtension($snow_url_target, $service{'serviceid'}, $int_version_key);
+
+  my($found_webhook_id);
+
+  # if found, get webhook id
+  if ($found_extension)
+  {
+    $found_webhook_id = $found_extension->{id};
+    print "Found ServiceNow Extension for $parm_service_target ($service{'serviceid'} $service{'epid'} $found_webhook_id)\n";
+  }
+  else
+  {
+    $found_webhook_id = createExtension($parm_snow_instance, $parm_snow_api_user, $parm_snow_api_pw, $parm_sync, $int_version_key, %service);
+    print "Created ServiceNow Extension for $parm_service_target ($service{'serviceid'} $service{'epid'} $found_webhook_id)\n";
+  }
+
+  #update SNOW with values
+  updateAssignmentGroup($parm_snow_update_user, $parm_snow_update_pw, $parm_snow_assignment_group_name, $parm_snow_instance, $found_webhook_id, %service);
 }
-
-#update SNOW with values
-updateAssignmentGroup($parm_snow_update_user, $parm_snow_update_pw, $parm_snow_assignment_group_name, $parm_snow_instance, $found_webhook_id, %service);
